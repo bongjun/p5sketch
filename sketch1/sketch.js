c36c5310f660265d42bb8019e19f510671bb6dcc
bongjun <bongjun@u.northwestern.edu>
2019-03-09 15:39:19 -0600

    updated

--- sketch1/sketch.js
+++ sketch1/sketch.js
@@ -2,14 +2,18 @@ var mic;
 
 var mySound;
 
+var delta=1;
+
+let newMicVol = 0;
+var growing = true;
 // function preload() {
 //   mySound = loadSound('HighwayRider.mp3');
 // }
 
 function setup(){
-	createCanvas(500, 500);
+  createCanvas(500, 500);
   //background(255, 204, 0);
-	
+  
 
   mic = new p5.AudioIn()
   mic.start();
@@ -23,31 +27,55 @@ function setup(){
   
 }
 function draw(){
-	background(255, 204, 0);
+  background(255, 204, 0);
   
-	micLevel = mic.getLevel();
-	strokeWeight(5)
-	let newMicVol = map(micLevel, 0, 0.03, 0, height);
-	//print(newMicVol)
+  micLevel = mic.getLevel();
+  strokeWeight(5)
+  
+  //let newMicVol = map(micLevel, 0, 0.03, 0, height);
+  //print(newMicVol)
+  
+  
+
+  if (newMicVol > height){
+    growing = false;
+    delta = -1;
+  }else if(newMicVol < 0){
+    growing = true;
+    delta = 1;
+  }
+  
+  // if (growing < height){
+  //   delta = 1
+  // }else{
+  //   delta = -1
+  // }
+  
+  newMicVol += 1*delta;
+  print(newMicVol);
   push()
-	ellipse(mouseX, mouseY, newMicVol, newMicVol);
+  ellipse(mouseX, mouseY, newMicVol, newMicVol);
   fill(0);
   ellipse(mouseX, mouseY, newMicVol*0.7, newMicVol*0.7);
-	fill(255);
+  fill(255);
   ellipse(mouseX, mouseY, newMicVol*0.4, newMicVol*0.4);
-	pop()
+  pop()
+  
+  
+  
+  
   //fill(10, 10, 10);
   //ellipe(mouseX, 200, newMicVol, newMicVol);
   
-  // push()
-  // let angle = map(micLevel, 0, 0.03, 0, 360);
+//   push()
+//   let angle = map(micLevel, 0, 0.03, 0, 360);
 
-  // angleMode(DEGREES);
-  // translate(width/2-50, height/2-50);
-  // rotate(angle);
-  // fill(134, 13, 255);
-  // rect(width/2-50, height/2-50, 100, 100)
-  // pop()
+//   angleMode(DEGREES);
+//   translate(width/2-50, height/2-50);
+//   rotate(angle);
+//   fill(134, 13, 255);
+//   rect(width/2-50, height/2-50, 100, 100)
+//   pop()
   
   
   //var spectrum = fft.analyze();
